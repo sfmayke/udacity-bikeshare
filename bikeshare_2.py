a37bf332edfd328605005f5b93daf75981706c1a
Mayke F. Santos <sfmayke@gmail.com>
2023-12-10 10:11:29 -0300

    bikeshare doc string text correction

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -99,8 +99,8 @@ def load_data(city):
 
     Args:
         (str) city - name of the city to analyze
-        (str) month - name of the month to filter by, or "all" to apply no month filter
-        (str) day - name of the day of week to filter by, or "all" to apply no day filter
+        (str) month - name of the month to filter by
+        (str) day - name of the day of week to filter by.
     Returns:
         df - Pandas DataFrame containing city data filtered by month and day
     """
